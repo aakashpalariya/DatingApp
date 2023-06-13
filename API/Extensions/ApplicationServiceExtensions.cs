8907e77ae8ef5d9fd3fa21c7bdd7224ed5b8d34b
Aakash Palariya <akashpalria@gmail.com>
2023-06-13 11:11:24 +0530

    Added mapper

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -26,6 +26,10 @@ namespace API.Extensions
             });
             services.AddScoped<ITokenService, TokenService>();
 
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
             return services;
 
         }
---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(dest=> dest.PhotoUrl, opt=>opt.MapFrom(src=>src.Photos.FirstOrDefault(s=>s.IsMain)))
+                .ForMember(dest=> dest.Age, opt=> opt.MapFrom(src=>src.DateOfBirth.CalculateAge()));
+            
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
