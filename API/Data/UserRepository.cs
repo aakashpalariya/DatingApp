ce37ef7b4c0008f7ffb817040d7fe54b1245cbdb
Aakash Palariya <akashpalria@gmail.com>
2023-06-17 09:46:01 +0530

    Sorting and last active status updated

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -21,18 +21,32 @@ namespace API.Data
 
         public async Task<MemberDto> GetMemberAsync(string username)
         {
-            return await _context.Users.Where(s=>s.UserName == username)
+            return await _context.Users.Where(s => s.UserName == username)
                 .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
                 .SingleOrDefaultAsync();
         }
 
         public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
-            var query = _context.Users
-                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                .AsNoTracking();
+            var query = _context.Users.AsQueryable();
 
-                return await PagedList<MemberDto>.CreateAsync(query, userParams.PageNumber, userParams.PageSize);
+            query = query.Where(u => u.UserName != userParams.CurrentUsername);
+            query = query.Where(u => u.Gender == userParams.Gender);
+
+            var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge - 1));
+            var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge - 1));
+
+            query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(u => u.Created),
+
+                _=> query.OrderByDescending(u=>u.LastActive)
+            };
+            return await PagedList<MemberDto>.CreateAsync(
+                query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider),
+                userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<AppUser> GetUserByIdAsync(int id)
@@ -43,14 +57,14 @@ namespace API.Data
         public async Task<AppUser> GetUserByUsernameAsync(string username)
         {
             return await _context.Users
-                .Include(p=>p.Photos)
-                .SingleOrDefaultAsync(s=>s.UserName == username);
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync(s => s.UserName == username);
         }
 
         public async Task<IEnumerable<AppUser>> GetUsersAsync()
         {
             return await _context.Users
-                .Include(p=>p.Photos)
+                .Include(p => p.Photos)
                 .ToListAsync();
         }
 
--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -1,5 +1,7 @@
 export interface User{
     username: string;
-    password: string;
+    token: string;
     photoUrl: string;
+    knownAs: string;
+    gender: string;
 }
