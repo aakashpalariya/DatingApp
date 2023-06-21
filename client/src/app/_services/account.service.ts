4132f3908bc17238664e401d38086d2dd616d611
Aakash Palariya <akashpalria@gmail.com>
2023-06-21 09:12:29 +0530

    Admin controller added for role management

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -14,21 +14,21 @@ export class AccountService {
 
   constructor(private http: HttpClient) { }
 
-  login(model: any){
+  login(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
-      map((response:User) =>{
+      map((response: User) => {
         const user = response;
-        if(user){
+        if (user) {
           this.setCurrentUser(user);
         }
       })
     )
   }
 
-  register(model:any){
-    return this.http.post<User>(this.baseUrl+'account/register',model).pipe(
-      map(user =>{
-        if(user){
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if (user) {
           this.setCurrentUser(user);
         }
         return user;
@@ -36,14 +36,21 @@ export class AccountService {
     )
   }
 
-  setCurrentUser(user: User){
+  setCurrentUser(user: User) {
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
-  logout(){
+  logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+  }
+
+  getDecodedToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]));
   }
 
 }
